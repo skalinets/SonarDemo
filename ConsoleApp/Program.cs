65b01895cc7eab01c7b357f5b556cab9f8345b15
Serhiy Kalinets <kalinets@gmail.com>
2018-04-11 19:00:28 +0300

    adds some code

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var d = new worker_class_with_bad_name().GetData();
+            Console.Out.WriteLine("d = {0}", d);
+        }
+    }
+
+    public class worker_class_with_bad_name
+    {
+        public string GetData()
+        {
+            return DateTime.Now.ToString();
         }
     }
 }
