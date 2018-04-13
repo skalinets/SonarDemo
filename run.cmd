3d8a828cf2301b943ad7ed005f2ee41f7c119493
Serhiy Kalinets <kalinets@gmail.com>
2018-04-13 15:51:15 +0300

    adds coverage

--- run.cmd
+++ run.cmd
@@ -1,7 +1,7 @@
+..\sonar-scanner\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.2" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml" /d:sonar.cs.opencover.reportsPaths="%CD%\results.xml"
 
-..\sonar-scanner\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.1" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml"
 msbuild /t:rebuild
-.\packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe .\ConsoleApp\bin\Debug\ConsoleApp.exe -xml "%CD%\XUnitResults.xml"
 
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:".\packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe" -targetargs:".\ConsoleApp\bin\Debug\ConsoleApp.exe -xml XUnitResults.xml" -register:user
 
 ..\sonar-scanner\SonarScanner.MSBuild.exe end
