41c8d041e563c95dc509e72cb9779f0af0e8b587
Serhiy Kalinets <kalinets@gmail.com>
2018-04-13 17:38:57 +0300

    adds resharper

--- .sonarqube/out/summary.md
+++ .sonarqube/out/summary.md
@@ -1,3 +1,3 @@
-Analysis succeeded for SonarQube project "Demo C# Project", version 1.7 [Analysis results](http://192.168.99.100:9000/dashboard/index/org.sonarqube:sonarqube-scanner-msbuild)
+Analysis succeeded for SonarQube project "Demo C# Project", version 1.8 [Analysis results](http://192.168.99.100:9000/dashboard/index/org.sonarqube:sonarqube-scanner-msbuild)
 - Product projects: 2, test projects: 0
 - Invalid projects: 0, skipped projects: 0, excluded projects: 0
--- run.cmd
+++ run.cmd
@@ -1,8 +1,9 @@
-..\sonar-scanner\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.7" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml" /d:sonar.cs.dotcover.reportsPaths="%CD%\dotCover.html"
+..\sonar-scanner\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.8" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml" /d:sonar.cs.dotcover.reportsPaths="%CD%\dotCover.html" /d:sonar.resharper.cs.reportPath="%CD%\resharper.xml" /d:sonar.resharper.solutionFile="%CD%\SonarDemo.sln"
 
 msbuild /t:rebuild
 
+".\packages\JetBrains.dotCover.CommandLineTools.2017.3.5\tools\dotCover.exe" analyse /ReportType=HTML /Output="%CD%\dotCover.html" /TargetExecutable=".\packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe" -TargetArguments:".\Our.Tests\bin\Debug\Our.Tests.dll -xmlv1 XUnitResults.xml" -Filter="-:module=xunit*"
 
-"%LOCALAPPDATA%\JetBrains\Installations\dotCover11\dotCover.exe" analyse /ReportType=HTML /Output="%CD%\dotCover.html" /TargetExecutable=".\packages\xunit.runner.console.2.3.1\tools\net452\xunit.console.exe" -TargetArguments:".\Our.Tests\bin\Debug\Our.Tests.dll -xmlv1 XUnitResults.xml" -Filter="-:module=xunit*"
+".\packages\JetBrains.ReSharper.CommandLineTools.2017.3.5\tools\inspectcode.exe" /output="%CD%\resharper.xml" "%CD%\SonarDemo.sln"
 
 ..\sonar-scanner\SonarScanner.MSBuild.exe end
