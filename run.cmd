26d588a83fb105d3e51816a7e193be73ee8d9037
Serhiy Kalinets <kalinets@gmail.com>
2018-05-22 12:24:11 +0300

    adds sonar scanner as nuget

--- run.cmd
+++ run.cmd
@@ -1,4 +1,6 @@
-..\sonar-scanner\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.17" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml" /d:sonar.cs.dotcover.reportsPaths="%CD%\dotCover.html" /d:sonar.resharper.cs.reportPath="%CD%\resharper.xml" /d:sonar.resharper.solutionFile="%CD%\SonarDemo.sln"
+nuget install MSBuild.SonarQube.Runner.Tool -Version 4.2.0
+
+.\MSBuild.SonarQube.Runner.Tool.4.2.0\tools\SonarScanner.MSBuild.exe begin /k:"org.sonarqube:sonarqube-scanner-msbuild" /n:"Demo C# Project" /v:"1.17" /d:"sonar.host.url=http://192.168.99.100:9000" /d:sonar.cs.xunit.reportsPaths="%CD%\XUnitResults.xml" /d:sonar.cs.dotcover.reportsPaths="%CD%\dotCover.html" /d:sonar.resharper.cs.reportPath="%CD%\resharper.xml" /d:sonar.resharper.solutionFile="%CD%\SonarDemo.sln"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\\MSBuild\15.0\bin\MSBuild.exe" /t:rebuild
 
@@ -6,4 +8,4 @@
 
 ".\packages\JetBrains.ReSharper.CommandLineTools.2017.3.5\tools\inspectcode.exe" /output="%CD%\resharper.xml" "%CD%\SonarDemo.sln"
 
-..\sonar-scanner\SonarScanner.MSBuild.exe end
+.\MSBuild.SonarQube.Runner.Tool.4.2.0\tools\SonarScanner.MSBuild.exe end
